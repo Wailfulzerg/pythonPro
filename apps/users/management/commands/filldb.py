274170b487a14341ae0a7a9a57b27d6b4b6be9be
Гаврилов Андрей <andrey.gavrilov@corp.mail.ru>
2021-05-13 14:30:10 +0300

    added users app, docker file, docker-compose

---
+++ apps/users/management/commands/filldb.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+
+class Command(BaseCommand):
+    users = [{'email': 'admin@admin.ru', 'username': 'admin'},
+             {'email': 'user1@user.ru', 'username': 'user1'},
+             {'email': 'user2@user.ru', 'username': 'user2'},
+             {'email': 'user3@user.ru', 'username': 'user3'}]
+    help = 'Create superuser admin and 3 users'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        for c_user in self.users:
+            user, created = User.objects.update_or_create(**c_user)
+            if c_user['username'] == 'admin':
+                user.is_staff = True
+                user.is_superuser = True
+            user.set_password(c_user['username'])
+            user.save()
+            if created:
+                self.stdout.write(
+                    self.style.SUCCESS(f"User {user.username} created.\nemail: {user.email}\npassword: {user.username}\n"))
+            else:
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"User {user.username} updated.\nemail: {user.email}\npassword: {user.username}\n"))
+
