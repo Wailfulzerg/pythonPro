adc281e47157e81dfdad5d7ed3a562790196b403
Гаврилов Андрей <andrey.gavrilov@corp.mail.ru>
2021-05-29 14:45:20 +0300

    fixex for hw4

--- apps/notes/filters.py
+++ apps/notes/filters.py
@@ -1,18 +1,18 @@
 from django_filters import rest_framework as filters
-from .models import Project
+from .models import Project, Note
 
 
 class ProjectFilter(filters.FilterSet):
-   title = filters.CharFilter(lookup_expr='contains')
+    title = filters.CharFilter(lookup_expr='contains')
 
-   class Meta:
-       model = Project
-       fields = ['title']
-
+    class Meta:
+        model = Project
+        fields = ['title']
 
 
 class NoteFilter(filters.FilterSet):
     project__name = filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
-        model = Product
+        model = Note
+        fields = ['project']
